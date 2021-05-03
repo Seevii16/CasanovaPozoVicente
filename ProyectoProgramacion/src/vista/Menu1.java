af134cea8bc9c4dfeef1a2da05f949e1fa2fad29
Seevii16 <sevillanito435@gmail.com>
2021-05-03 16:37:39 +0200

    Cambios en el registro y menu1

--- ProyectoProgramacion/src/vista/Menu1.java
+++ ProyectoProgramacion/src/vista/Menu1.java
@@ -9,6 +9,11 @@ import javax.swing.border.EmptyBorder;
 import javax.swing.JLabel;
 import javax.swing.ImageIcon;
 import java.awt.Font;
+import java.awt.Toolkit;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.awt.Color;
 
 public class Menu1 extends JFrame {
 
@@ -34,12 +39,52 @@ public class Menu1 extends JFrame {
 	 * Create the frame.
 	 */
 	public Menu1() {
+		setTitle("Menu");
+		setIconImage(Toolkit.getDefaultToolkit().getImage("C:\\Users\\Usuario\\Documents\\GitHub\\CasanovaPozoVicente\\ProyectoProgramacion\\src\\imagenes\\icono.png"));
 		setFont(new Font("Comic Sans MS", Font.PLAIN, 13));
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 707, 409);
+		setBounds(100, 100, 493, 368);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
+		
+		JPanel panel = new JPanel();
+		panel.setBounds(0, 0, 308, 329);
+		contentPane.add(panel);
+		panel.setLayout(null);
+		
+		JButton btnRegistro = new JButton("Registro\r\n");
+		btnRegistro.setBounds(318, 185, 149, 35);
+		btnRegistro.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				Registro frame = new Registro();
+			}
+		});
+		contentPane.add(btnRegistro);
+		
+		JButton btnNewButton = new JButton("Inicio de Sesion\r\n");
+		btnNewButton.setBounds(318, 106, 149, 35);
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				InicioSesion frame = new InicioSesion();
+				frame.setVisible(true);
+			}
+		});
+		contentPane.add(btnNewButton);
+		
+		JLabel lblNewLabel = new JLabel("\r\n");
+		lblNewLabel.setIcon(new ImageIcon("C:\\Users\\Usuario\\Documents\\GitHub\\CasanovaPozoVicente\\ProyectoProgramacion\\src\\imagenes\\rengoku.gif"));
+		lblNewLabel.setBounds(0, 0, 308, 329);
+		panel.add(lblNewLabel);
+		
+		
+		JLabel lblNewLabel_1 = new JLabel("\r\n");
+		lblNewLabel_1.setIcon(new ImageIcon("C:\\Users\\Usuario\\Documents\\GitHub\\CasanovaPozoVicente\\ProyectoProgramacion\\src\\imagenes\\humo roj.jpg"));
+		lblNewLabel_1.setBounds(310, 0, 167, 329);
+		contentPane.add(lblNewLabel_1);
+	
+	
+		
 	}
 }
--- ProyectoProgramacion/src/vista/Registro.java
+++ ProyectoProgramacion/src/vista/Registro.java
@@ -8,10 +8,19 @@ import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import java.awt.Font;
 import java.awt.Toolkit;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.JPasswordField;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class Registro extends JFrame {
 
 	private JPanel contentPane;
+	private JTextField textField;
+	private JTextField textField_1;
+	private JPasswordField textField_2;
 
 	/**
 	 * Launch the application.
@@ -37,11 +46,56 @@ public class Registro extends JFrame {
 		setFont(new Font("Comic Sans MS", Font.PLAIN, 13));
 		setTitle("Registro");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 607, 387);
+		setBounds(100, 100, 641, 425);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JButton btnNewButton = new JButton("Atras\r\n");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				Menu1 frame = new Menu1();
+				frame.setVisible(true);
+			}
+		});
+		btnNewButton.setFont(new Font("Comic Sans MS", Font.PLAIN, 13));
+		btnNewButton.setBounds(10, 343, 79, 32);
+		contentPane.add(btnNewButton);
+		
+		JLabel lblNewLabel = new JLabel("Nombre\r\n");
+		lblNewLabel.setFont(new Font("Comic Sans MS", Font.PLAIN, 13));
+		lblNewLabel.setBounds(130, 55, 55, 24);
+		contentPane.add(lblNewLabel);
+		
+		textField = new JTextField();
+		textField.setBounds(213, 59, 251, 20);
+		contentPane.add(textField);
+		textField.setColumns(10);
+		
+		JLabel lblEmail = new JLabel("Email\r\n");
+		lblEmail.setFont(new Font("Comic Sans MS", Font.PLAIN, 13));
+		lblEmail.setBounds(130, 108, 55, 24);
+		contentPane.add(lblEmail);
+		
+		textField_1 = new JTextField();
+		textField_1.setColumns(10);
+		textField_1.setBounds(213, 112, 251, 20);
+		contentPane.add(textField_1);
+		
+		JLabel lblContrasea = new JLabel("Contrase\u00F1a\r\n");
+		lblContrasea.setFont(new Font("Comic Sans MS", Font.PLAIN, 13));
+		lblContrasea.setBounds(130, 158, 79, 24);
+		contentPane.add(lblContrasea);
+		
+		textField_2 = new JPasswordField();
+		textField_2.setColumns(10);
+		textField_2.setBounds(213, 160, 251, 20);
+		contentPane.add(textField_2);
+		
+		JButton btnNewButton_1 = new JButton("Siguiente\r\n");
+		btnNewButton_1.setFont(new Font("Comic Sans MS", Font.PLAIN, 13));
+		btnNewButton_1.setBounds(444, 220, 98, 32);
+		contentPane.add(btnNewButton_1);
 	}
-
 }
