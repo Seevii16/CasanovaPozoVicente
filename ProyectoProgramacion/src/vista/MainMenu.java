f506f8a0436231fa7426d5e2bdacf95875e69df7
Seevii16 <sevillanito435@gmail.com>
2021-05-05 18:44:04 +0200

    Cambios en el main menu

--- ProyectoProgramacion/src/vista/MainMenu.java
+++ ProyectoProgramacion/src/vista/MainMenu.java
@@ -48,33 +48,62 @@ public class MainMenu extends JFrame {
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
-		JButton btnNewButton = new JButton("New button");
+		JButton btnNewButton = new JButton("Shonen\r\n");
 		btnNewButton.setFont(new Font("Comic Sans MS", Font.PLAIN, 13));
 		btnNewButton.setBounds(22, 82, 164, 57);
 		contentPane.add(btnNewButton);
-		JButton btnNewButton_1 = new JButton("New button");
+		JButton btnNewButton_1 = new JButton("Shojo\r\n");
 		btnNewButton_1.setFont(new Font("Comic Sans MS", Font.PLAIN, 13));
 		btnNewButton_1.setBounds(22, 152, 164, 57);
 		contentPane.add(btnNewButton_1);
 		
-		JButton btnNewButton_2 = new JButton("New button");
+		JButton btnNewButton_2 = new JButton("Seinen\r\n");
 		btnNewButton_2.setFont(new Font("Comic Sans MS", Font.PLAIN, 13));
 		btnNewButton_2.setBounds(22, 220, 164, 57);
 		contentPane.add(btnNewButton_2);
 		
-		JButton btnNewButton_1_1 = new JButton("New button");
+		JButton btnNewButton_1_1 = new JButton("Terror");
 		btnNewButton_1_1.setFont(new Font("Comic Sans MS", Font.PLAIN, 13));
 		btnNewButton_1_1.setBounds(22, 286, 164, 57);
 		contentPane.add(btnNewButton_1_1);
 		
-		JButton btnNewButton_2_1 = new JButton("New button");
+		JButton btnNewButton_2_1 = new JButton("Comedia\r\n");
 		btnNewButton_2_1.setFont(new Font("Comic Sans MS", Font.PLAIN, 13));
 		btnNewButton_2_1.setBounds(22, 354, 164, 57);
 		contentPane.add(btnNewButton_2_1);
 		
 		JButton btnNewButton_3 = new JButton("");
-		btnNewButton_3.setBounds(253, 82, 141, 222);
+		btnNewButton_3.setIcon(new ImageIcon("C:\\Users\\Usuario\\Documents\\GitHub\\CasanovaPozoVicente\\ProyectoProgramacion\\src\\imagenes\\Kimetsu_no_Yaiba.png"));
+		btnNewButton_3.setBounds(206, 82, 141, 222);
 		contentPane.add(btnNewButton_3);
+		
+		JButton btnNewButton_3_1 = new JButton("");
+		btnNewButton_3_1.setIcon(new ImageIcon("C:\\Users\\Usuario\\Documents\\GitHub\\CasanovaPozoVicente\\ProyectoProgramacion\\src\\imagenes\\One Piece.png"));
+		btnNewButton_3_1.setBounds(368, 82, 141, 222);
+		contentPane.add(btnNewButton_3_1);
+		
+		JButton btnNewButton_3_2 = new JButton("");
+		btnNewButton_3_2.setIcon(new ImageIcon("C:\\Users\\Usuario\\Documents\\GitHub\\CasanovaPozoVicente\\ProyectoProgramacion\\src\\imagenes\\dragon_ball_super.jpg"));
+		btnNewButton_3_2.setBounds(531, 82, 141, 222);
+		contentPane.add(btnNewButton_3_2);
+		
+		JLabel lblNewLabel_1_2 = new JLabel("Kimetsu no yaiba\r\n");
+		lblNewLabel_1_2.setForeground(Color.WHITE);
+		lblNewLabel_1_2.setFont(new Font("Comic Sans MS", Font.PLAIN, 15));
+		lblNewLabel_1_2.setBounds(216, 286, 131, 57);
+		contentPane.add(lblNewLabel_1_2);
+		
+		JLabel lblNewLabel_1_2_1 = new JLabel("One Piece\r\n");
+		lblNewLabel_1_2_1.setForeground(Color.WHITE);
+		lblNewLabel_1_2_1.setFont(new Font("Comic Sans MS", Font.PLAIN, 15));
+		lblNewLabel_1_2_1.setBounds(402, 286, 85, 57);
+		contentPane.add(lblNewLabel_1_2_1);
+		
+		JLabel lblNewLabel_1_2_1_1 = new JLabel("Dragon ball Super\r\n");
+		lblNewLabel_1_2_1_1.setForeground(Color.WHITE);
+		lblNewLabel_1_2_1_1.setFont(new Font("Comic Sans MS", Font.PLAIN, 15));
+		lblNewLabel_1_2_1_1.setBounds(541, 286, 131, 57);
+		contentPane.add(lblNewLabel_1_2_1_1);
 		
 		JLabel lblNewLabel_1 = new JLabel("Generos\r\n");
 		lblNewLabel_1.setFont(new Font("Comic Sans MS", Font.PLAIN, 15));
@@ -90,8 +119,10 @@ public class MainMenu extends JFrame {
 		
 		JLabel lblNewLabel = new JLabel("");
 		lblNewLabel.setIcon(new ImageIcon("C:\\Users\\Usuario\\Documents\\GitHub\\CasanovaPozoVicente\\ProyectoProgramacion\\src\\imagenes\\fondo_mainMenu.jpg"));
-		lblNewLabel.setBounds(0, 0, 685, 427);
+		lblNewLabel.setBounds(10, 0, 695, 437);
 		contentPane.add(lblNewLabel);
+		
+		
 		
 		
 		
